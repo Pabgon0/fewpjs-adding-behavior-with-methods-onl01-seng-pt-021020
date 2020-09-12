d9c4cd542dc34d21c3dee693b747c0bbe0264d2f
Pablo Gonzalez <pabgon0@gmail.com>
2020-09-12 05:02:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,11 @@ class Dog {
  
 class Bird {
   constructor(name, sex) {
-    if (this.sex === "male") {
+    this.name = name;
+    this.sex =  sex;
+  }
+    speak() {
+      if (this.sex === "male") {
       let speak = `Its me! ${name}, the parrot!`
     } else {
       let speak = `${name} says squawk!`
